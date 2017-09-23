c6a430a7f63d45cea662714918495e3520a5e66e
wkdgudcjf1 <wkdgudcjf1@naver.com>
2017-09-23 19:37:25 +0900

    [front-end] 1st commit

--- src/main/java/com/example/category/controller/WebController.java
+++ src/main/java/com/example/category/controller/WebController.java
@@ -12,10 +12,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/web")
 public class WebController {
 
-    @RequestMapping(value = "/test", method = RequestMethod.GET)
-    public String index(Model model) {
+    @RequestMapping(value = {"/", "/login"}, method = RequestMethod.GET)
+    public String login(Model model) {
         model.addAttribute("test", "test");
-        return "test";
+        return "login";
     }
-
+    @RequestMapping(value = {"/agreement"}, method = RequestMethod.GET)
+    public String agreement(Model model) {
+        model.addAttribute("test", "test");
+        return "agreement";
+    }
+    @RequestMapping(value = "/join", method = RequestMethod.GET)
+    public String join(Model model) {
+        model.addAttribute("test", "test");
+        return "join";
+    }
 }
